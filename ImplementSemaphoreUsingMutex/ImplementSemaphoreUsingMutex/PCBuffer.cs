9fb412628fe308a8b807b36c516ebcfdf0cbf368
enzojack <lc.mihut@gmail.com>
2018-03-07 22:28:26 +0200

    Semaphore doesn t work

---
+++ ImplementSemaphoreUsingMutex/ImplementSemaphoreUsingMutex/PCBuffer.cs
@@ -0,0 +1,62 @@
+﻿using ImplementSemaphoreUsingMutex;
+using System;
+
+
+namespace ProducerConsumerUsingSemaphores
+{
+    public class PCBuffer
+    {
+
+        private Semaphore semaProd;
+        private Semaphore semaCons;
+        private int[] buffer;
+        private int bufferHead, bufferTail;
+        private volatile int noElementsInBuffer;
+        private Random rand;
+
+        public PCBuffer(int bufferSize)
+        {
+            buffer = new int[bufferSize];
+            bufferHead = 0;
+            bufferTail = 0;
+            noElementsInBuffer = 0;
+            semaProd = new Semaphore(bufferSize);
+            semaCons = new Semaphore();
+            rand = new Random();
+        }
+
+        public int PopElementFromBuffer(int consumerId)
+        {
+            int element = 0;
+
+            semaCons.Aquire();
+            element = buffer[bufferHead % buffer.Length];
+            Console.WriteLine("Consumer {2} pop from buffer the element with value {0} and index {1}", element, bufferHead, consumerId);
+            bufferHead++;
+
+            Console.WriteLine(noElementsInBuffer);
+            semaProd.Release();
+
+
+            return element;
+        }
+
+        public void PushElementInBuffer(int id)
+        {
+
+            semaProd.Aquire();
+            int element = rand.Next(1, 100);
+            buffer[bufferTail % buffer.Length] = element;
+            Console.WriteLine("Producer {2} push in buffer the element with value {0} and index {1} ", element, bufferTail, id);
+            bufferTail++;
+
+            Console.WriteLine(noElementsInBuffer);
+
+            semaCons.Release();
+
+
+        }
+
+
+    }
+}
---
+++ ImplementSemaphoreUsingMutex/ImplementSemaphoreUsingMutex/Semaphore.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ImplementSemaphoreUsingMutex
+{
+    class Semaphore
+    {
+        private volatile int permits;
+        private Mutex permitsMutex;
+        private Mutex waitForPermit;
+
+        public Semaphore(int nrOfPermits = 0)
+        {
+            permits = nrOfPermits;
+            permitsMutex = new Mutex();
+            waitForPermit = new Mutex();
+        }
+
+
+        public void Aquire()
+        {
+            permitsMutex.WaitOne();
+            while(permits == 0)
+            {
+                waitForPermit.WaitOne();
+            }
+            permits--;
+            permitsMutex.ReleaseMutex();
+        }
+
+        public void Release()
+        {
+            permitsMutex.WaitOne();
+            if(permits == 0)
+            {
+                
+                waitForPermit.ReleaseMutex();
+            }
+            permits++;
+            permitsMutex.ReleaseMutex();
+        }
+    }
+
+    
+}
